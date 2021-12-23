9a5891de4f3b853f0aa723f2fcc1f969f0498662
Leandro Vicente <leandrovicente.info@gmail.com>
2021-12-23 14:49:59 -0300

    Update description on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,10 +10,10 @@ class Home extends Component {
   render() {
     return(
       <React.Fragment>
-        <Bio name='Leandro Vicente' tagline='Software Developer and Content Creator' />
+        <Bio name='Leandro Vicente' tagline='Creator and Product Analyst' />
         <SocialBox iconsAndLinks={SOCIAL_ICONS_AND_LINKS} />
         <ContactBox /> 
-        <Footer address='Belo Horizonte - MG, Brazil' />
+        <Footer address='Rio de Janeiro, Brazil' />
       </React.Fragment>
     )
   }
